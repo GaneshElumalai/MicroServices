1a107b464858d7453ba1da2f8cf97ece24da94a4
GaneshElumalai <ganeshes24@gmail.com>
2019-09-26 10:33:29 +0530

    Refactored

--- SocializeApp/src/main/java/com/socialize/controller/SocializeController.java
+++ SocializeApp/src/main/java/com/socialize/controller/SocializeController.java
@@ -1,12 +1,12 @@
 package com.socialize.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SocializeController {
 	
-	@GetMapping("/message")
+	@RequestMapping("/message")
 	public String test() {
 		return "Posted";
 	}
